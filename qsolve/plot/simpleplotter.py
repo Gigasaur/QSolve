e48e19fe4209debce21d1c5f0d563b476cbbc08e
Gigasaur <wrmagroganiii@gmail.com>
2017-01-16 17:08:42 -0700

    setup

---
+++ qsolve/plot/simpleplotter.py
@@ -0,0 +1,31 @@
+""" This module encompasses the logic needed
+to plot and display the data from the rest of 
+the code base
+
+"""
+
+###########
+# Imports # 
+###########
+
+#####################
+# Class definitions #
+#####################
+
+def Plotter(object):
+    """ This is a base class for
+    subsequent plotters to inherit from
+
+    """
+
+    def __init__(self):
+        """ Placeholder docstring
+
+        """
+        pass
+
+    def plot(cls):
+        """ Placeholder docstring
+
+        """
+        pass
