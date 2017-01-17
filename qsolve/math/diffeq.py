dec99e3b41a0d047fefb91b134ef3dbf9147da98
Gigasaur <wrmagroganiii@gmail.com>
2017-01-16 17:01:39 -0700

    setup

---
+++ qsolve/math/diffeq.py
@@ -0,0 +1,47 @@
+""" This module encompasses all the methods
+needed to solve ordinary differential equations
+
+"""
+
+###########
+# Imports #
+###########
+
+#####################
+# Class definitions #
+#####################
+
+class DifferentialEquationMethod(object):
+    """ Placeholder doc
+
+    """
+    def __init__(self):
+        """ Placeholder docstring
+
+        """
+        pass
+
+    def solve(cls):
+        """ Placeholder docstring
+
+        """
+        pass
+
+class RungeKutta(DifferentialEquationMethod):
+    """ This class will implement the 
+    Runge Kutta method of numerically
+    solving differential equations
+
+    """
+    
+    def __init__(self):
+        """ Placeholder docstring
+
+        """
+        pass
+
+    def solve(cls):
+        """ Placeholder docstring
+
+        """
+        pass
